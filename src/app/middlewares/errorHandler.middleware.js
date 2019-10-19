7b28cc4935d1e2cbdf0198885880caedf001ce41
Fabricio Farias <fabriciofarias@id.uff.br>
2019-10-19 20:58:45 -0300

    Fix errorHandler

--- src/app/middlewares/errorHandler.middleware.js
+++ src/app/middlewares/errorHandler.middleware.js
@@ -1,6 +1,5 @@
  
 module.exports = (err, req, res, next) => {
   const { status, message } = err;
-  console.error(error);
   return res.status(status || 500).send({ error: message });
 };
--- src/app/middlewares/routeNotFound.middleware.js
+++ src/app/middlewares/routeNotFound.middleware.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log('entrou')
   const error = new Error('Route Not Found');
   error.status = 404;
 
