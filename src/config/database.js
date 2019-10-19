bcab54973d18504ece101787341cf086ee01c927
Fabricio Farias <fabriciofarias@id.uff.br>
2019-10-19 13:06:18 -0300

    Setting up sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'gollum',
+  password: 'my_precious',
+  database: 'mid_dev',
+  dialect: 'mysql',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
